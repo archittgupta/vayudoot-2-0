d2d428b6b1b34f79cf69aab2996370c50762317c
Archit Gupta <architgupta539@gmail.com>
2025-02-27 01:38:24 +0530

    im fixing deployment issues

--- src/app/components/aurora-background.tsx
+++ src/app/components/aurora-background.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "../lib/utils";
+import { cn } from "../../lib/utils";
 import React, { ReactNode } from "react";
 import Link from "next/link";
 import Navbar from "./navbar";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Backdrop } from "../components/backdrop";
-import CountdownTimer from "../components/countdown";
+import { Backdrop } from "./components/backdrop";
+import CountdownTimer from "./components/countdown";
 import React, { useState } from "react";
-import Roadmap from "../components/roadmap";
+import Roadmap from "./components/roadmap";
 
-import Footer from "../components/footer";
+import Footer from "./components/footer";
 
 
 
