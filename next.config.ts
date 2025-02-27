f4639d32b4d0418be09eb0a491ad99362a5c143a
Archit Gupta <architgupta539@gmail.com>
2025-02-28 04:19:00 +0530

    eslint : true added in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['assets.aceternity.com', 'images.unsplash.com'], // Added Unsplash images support
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
